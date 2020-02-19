5cf8c8ba338e87a7f80ec63e60512e1cec3dae75
Nagaraju Pagoti <nagaraju.pagoti@testingxperts.loc>
2020-02-19 11:13:14 +0530

    alt

---
+++ anty/src/test/java/anty2/Anty.java
@@ -0,0 +1,13 @@
+package anty2;
+
+import org.testng.annotations.Test;
+
+public class Anty {
+    @Test
+	public static void man() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the world 1");
+
+	}
+
+}
